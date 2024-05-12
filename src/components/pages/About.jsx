53bef32ddd8f74a9326271acb3b942efce1325de
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-05-11 21:11:45 -0600

    More content added to about me

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,60 @@
 export default function About() {
     return (
         <div>
-            About goes here
+            <h1>Hello. My name is Jordan Heersink. Thank you so much for visiting my site! Please scroll down to learn more about me, and use the navigation buttons for more of my web development resources. </h1>
+            <img src='src\components\assets\headshot.PNG' />
+            <p>I am a full-stack web developer seeking to apply my eight years of tech support management expertise to the web. After spending time learning how end users think and knowing what they look for, I want to hone my web development skills into creating a user experience that aligns with their wants and needs. I am enthusiastic about technology and always eager to learn more. I have a BA in Information Systems Management, a master's degree in Business Administration, and I recently acquired a web development certificate through a rigorous course at Denver University. I'm looking to bring my commitment to excellence and hard work ethic to a company and become a valuable asset.
+            </p>
+            <h2>My Milestones:</h2>
+            <ul>
+                <li>2010 - Married the Best Person on Planet Earth</li>
+                <li>2014 - Graduated with a BS in Information Systems Management</li>
+                <li>2015 - Entered the IT Field as an Assistant Vice President of Technical Support</li>
+                <li>2017 - Obtained my MBA with an Emphasis in Leadership</li>
+                <li>2023 - My Wife and I had a Baby Boy</li>
+                <li>2023 - Entered a Full Stack Web Development Bootcamp</li>
+                <li>2024 - Completed Web Development Bootcamp Certificate</li>
+            </ul>
+            <h2>My Skills:</h2>
+            <p>These are some of the items in my toolbelt.</p>
+            <ul>
+                <li>Git</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>Web Services APIs</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>PostgreSQL</li>
+                <li>MongoDB</li>
+                <li>React</li>
+            </ul>
+
+            <h2>What's Next for Me:</h2>
+            <p>These are some of the things I want to learn next.</p>
+            <ul>
+                <li>Programming Languages</li>
+                <ul>
+                    <li>C#</li>
+                    <li>Python</li>
+                    <li>PHP</li>
+                    <li>TypeScript</li>
+                </ul>
+                <li>Libraries & Frameworks</li>
+                <ul>
+                    <li>D3</li>
+                    <li>Next.js</li>
+                    <li>Tailwind CSS</li>
+                </ul>
+                <li>Tools & Platforms</li>
+                <ul>
+                    <li>AWS</li>
+                    <li>GraphQL Playground</li>
+                    <li>Docker</li>
+                </ul>
+            </ul>
         </div>
     )
 }
