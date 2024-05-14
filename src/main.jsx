558d67bf40677b31bc667a5fa74cdf597826f2da
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-05-13 18:18:29 -0600

    CSS minus animations complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// import ScrollMagic from 'scrollmagic';
+// import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'; // This imports the GSAP plugin for ScrollMagic
+// import gsap from 'gsap';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
