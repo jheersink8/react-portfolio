841ac033ef2d455b02b26efbe900436fe94dbb31
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-05-15 23:14:18 -0600

    98% of polish complete.

--- src/components/tools/TimelineEvents.jsx
+++ src/components/tools/TimelineEvents.jsx
@@ -2,7 +2,7 @@ function TimelineEvents(props) {
     return (
         <ul className="timeline">
             {props.timelineEvents.map(event => (
-                <li className="event" data-date={event.date} key={event.id}>
+                <li className="event scrollTimeline" data-date={event.date} key={event.id}>
                                       
                         <div>{event.description}</div>
              
